e44b9f68447edc6bb21839f5334282f96114d84b
Nvcuonghandsome <84920803+Nvcuonghandsome@users.noreply.github.com>
2022-05-29 10:13:46 +0700

    view engine config

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import configViewEngine from './configs/viewEngine'
+
+const app = express()
+const port = 3000
+
+configViewEngine(app)
+
+app.get('/', (req, res) => {
+    res.render('test/index.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.send(`I'm Cuong...`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
