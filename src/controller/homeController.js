6bfddae1414df8e97e173b93afa6ca1bbb8ec5dd
Nvcuonghandsome <84920803+Nvcuonghandsome@users.noreply.github.com>
2022-06-08 12:09:51 +0700

    form and post request

---
+++ src/controller/homeController.js
@@ -0,0 +1,23 @@
+import pool from '../configs/connectDB'
+
+let getHomepage = async (req, res) => {
+
+    const [rows, fields] = await pool.execute('SELECT * FROM users')
+    return res.render('index.ejs', { dataUser: rows })
+}
+
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id
+    let [user] = await pool.execute('SELECT * FROM users where id = ?', [userId])
+    return res.send(JSON.stringify(user))
+}
+
+let createNewUser = async (req, res) => {
+    let { firstName, lastName, email, address } = req.body
+    await pool.execute('INSERT INTO users(firstName, lastName, email, address) VALUES(?, ?, ?, ?)', [firstName, lastName, email, address])
+    return res.redirect('/')
+}
+
+module.exports = {
+    getHomepage, getDetailPage, createNewUser
+}
--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+import initWebRoute from './route/web'
+// import connection from './configs/connectDB'
+
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// setup view engine
 configViewEngine(app)
 
-app.get('/', (req, res) => {
-    res.render('test/index.ejs')
-})
-
-app.get('/about', (req, res) => {
-    res.send(`I'm Cuong...`)
-})
+// init web route
+initWebRoute(app)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
